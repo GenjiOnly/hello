7167044be377e76090593a43f35a0bdc0b8e45bc
deenrookie <deen@MacBook-Pro.local>
2019-07-22 20:01:18 +0800

    Add files

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+  <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">
+      <NavMenu />
+    </el-aside>
+    <el-main>
+      <vue-command />
+    </el-main>
+  </el-container>
+  </el-container>
+  </div>
+</template>
+
+<script>
+import VueCommand from './components/VueCommand.vue'
+import NavMenu from './components/NavMenu.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VueCommand,
+    NavMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
